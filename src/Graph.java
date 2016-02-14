f435aea99a642ba44e95cf90c1c64217072651a0
amaridev <amaridev@ceyu.de>
2016-02-14 19:37:27 +0100

    pathfinding

--- src/Graph.java
+++ src/Graph.java
@@ -13,11 +13,24 @@ public class Graph {
     private List<Pair<Integer, Integer>>[] adjacency;
     private String[] labels;
 
+    //Pathfinding
+    private int[] dist;
+    private boolean[] visited;
+    private int[] previous;
+
     private final int MAX_WEIGHT = 50;
 
     public Graph(int nrOfvertices) {
         this.adjacency = new LinkedList[nrOfvertices];
         this.labels = new String[nrOfvertices];
+
+        this.dist = new int[nrOfvertices];
+        this.previous = new int[nrOfvertices];
+        this.visited = new boolean[nrOfvertices];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        Arrays.fill(visited, false);
+        Arrays.fill(previous, -1);
+
 
         for (int i = 0; i < adjacency.length; i++) {
             adjacency[i] = new LinkedList<>();
@@ -42,9 +55,33 @@ public class Graph {
         return adjacency[startVertex].remove(edge);
     }
 
-    public void getNeighbour() {
+    public void pathfinder(int start, int end) {
+        dist[start] = 0;
+
+        /*
+        insert start into Q
+
+        while Q is not empty
+            u := vertex smallest dist && !visited
+            remove u from Q
+            visited[u] := true
+
+            for each neighbour v of u
+                alt := dist[u] + dist(u, v)
+                if alt < dist[v]
+                    dist[v] := alt
+                    previous[v] := u
+                    if !visited[u]
+                        insert v into Q
+                    end if
+                end if
+            end for-each
+        end while
+        return dist...
+         */
 
     }
+
 
     private void getRandomGraph(int maxEdges) {
         ArrayList<Pair<Integer, Integer>> edge = IntStream.range(0, maxEdges)
